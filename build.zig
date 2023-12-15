d05d419edfb62e0fba5f4b00568728dc3ab34610
fuzhou.chen <fuzhouch@gmail.com>
2023-12-15 12:50:25 +0800

    [NEW] Add initial project structure.

---
+++ build.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+
+// ====================================================================
+// The target definition and gba.ld are from @wendigojaeger's project
+// https://github.com/wendigojaeger/ZigGBA
+//
+// Thank you so much for the sharing!
+const gba_thumb_target = blk: {
+    var target = std.zig.CrossTarget {
+        .cpu_arch = std.Target.Cpu.Arch.thumb,
+        .cpu_model = . { .explicit = &std.Target.arm.cpu.arm7tdmi },
+        .os_tag = .freestanding,
+    };
+
+    target.cpu_features_add.addFeature(@intFromEnum(std.Target.arm.Feature.thumb_mode));
+    break :blk target;
+};
+
+fn libRoot() []const u8 {
+    return std.fs.path.dirname(@src().file) orelse ".";
+}
+const GBALinkerScript = libRoot() ++ "/src/gba.ld";
+
+// ====================================================================
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = b.addStaticLibrary(.{
+        .name = "zamgba",
+        .root_source_file = .{ .path = "src/lib.zig" },
+        .target = gba_thumb_target,
+        .optimize = optimize,
+    });
+    lib.setLinkerScriptPath(std.build.FileSource { .path = GBALinkerScript });
+    b.installArtifact(lib);
+
+    // TODO Examples are built as .gba files, which suppose to be
+    // executed on a GBA device or emulator. Thus, we don't
+    // support running via `zig build run ...'.
+
+    // TODO Though not sure whether doable, let's keep unit test anyway.
+    // Some logic should be able to run on devbox.
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/lib.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+}
