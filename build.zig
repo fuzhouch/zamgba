4679b6d61c33695cf578773e00ffd9d789cb1537
fuzhou.chen <fuzhouch@gmail.com>
2024-01-14 16:35:54 +0800

    [FIX] Migrate to Zig 0.12.0-dev.2150+63de8a598 (20230114).
    1. Disable building of C code. Seems zig does not automatically detect C
       code anymore.
    2. Use std.Build.ResolvedTarget to fit new build.zig build coding
       pattern.

--- build.zig
+++ build.zig
@@ -4,16 +4,16 @@ const std = @import("std");
 // The target definition and gba.ld are from @wendigojaeger's project.
 // https://github.com/wendigojaeger/ZigGBA
 //
-const gba_thumb_target = blk: {
-    var target = std.zig.CrossTarget {
+
+fn buildGBAThumbTarget(b: *std.Build) std.Build.ResolvedTarget {
+    var query = std.zig.CrossTarget {
         .cpu_arch = std.Target.Cpu.Arch.thumb,
         .cpu_model = . { .explicit = &std.Target.arm.cpu.arm7tdmi },
         .os_tag = .freestanding,
     };
-
-    target.cpu_features_add.addFeature(@intFromEnum(std.Target.arm.Feature.thumb_mode));
-    break :blk target;
-};
+    query.cpu_features_add.addFeature(@intFromEnum(std.Target.arm.Feature.thumb_mode));
+    return std.Build.resolveTargetQuery(b, query);
+}
 
 fn libRoot() []const u8 {
     return std.fs.path.dirname(@src().file) orelse ".";
@@ -23,7 +23,7 @@ const GBALinkerScript = libRoot() ++ "/src/gba.ld";
 // ====================================================================
 
 fn addDemo(b: *std.Build,
-           target: std.zig.CrossTarget,
+           target: std.Build.ResolvedTarget,
            optimize: std.builtin.OptimizeMode,
            executable: []const u8,
            sourceRoot: []const u8) void {
@@ -33,13 +33,14 @@ fn addDemo(b: *std.Build,
         .target = target,
         .optimize = optimize,
     });
-    demo.setLinkerScriptPath(std.build.FileSource { .path = GBALinkerScript });
+    demo.setLinkerScriptPath(std.Build.LazyPath { .path = GBALinkerScript });
     b.installArtifact(demo);
 }
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const gba_thumb_target = buildGBAThumbTarget(b);
 
     const lib = b.addStaticLibrary(.{
         .name = "zamgba",
@@ -47,12 +48,15 @@ pub fn build(b: *std.Build) void {
         .target = gba_thumb_target,
         .optimize = optimize,
     });
-    lib.setLinkerScriptPath(std.build.FileSource { .path = GBALinkerScript });
+    lib.setLinkerScriptPath(std.Build.LazyPath { .path = GBALinkerScript });
     b.installArtifact(lib);
 
     // All demos - Note that all executables must be run via emulator.
     addDemo(b, gba_thumb_target, optimize, "first.elf", "demo/first.zig");
-    addDemo(b, gba_thumb_target, optimize, "first.c.elf", "demo/first.c");
+    // TODO: Since 0.12.0-dev.2150+63de8a598, build.zig does not
+    // automatically detect C code automatically. Haven't found a good
+    // way to do it. Let's disable it for now.
+    // addDemo(b, gba_thumb_target, optimize, "first.c.elf", "demo/first.c");
 
     // TODO Though not sure whether doable, let's keep unit test anyway.
     // Some logic should be able to run on devbox.
