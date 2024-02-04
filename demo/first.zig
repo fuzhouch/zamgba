fd707dcbd29719a635796e2327d484f340286160
fuzhou.chen <fuzhouch@gmail.com>
2024-02-04 21:34:28 +0800

    [DOC] Update documentation to include build steps on lib and executable.

--- demo/first.zig
+++ demo/first.zig
@@ -1,12 +1,18 @@
 const gba = @import("gba");
 
-// A .header is required to be put at the beginning of GBA rom
+// The gameHeader is required at the beginning of GBA rom
 // with correct game name, game code, maker code and version.
-// In devkitARM, this step not done in build time, but done by
-// gbafix. We learn from ZigGBA to build it manually.
+// It can't be initialized within main(), because GBA BIOS relies on
+// the header to locate main().
 //
-// I'm looking for a way to completely remove this statement, leaving
-// only main() in SDK users' code.
+// In devkitARM, this step not done in build time, but done by
+// gbafix. This approach is learnt from ZigGBA. It allows we build
+// everything in code, intead of requiring an additional gbafix.
+//
+// Note that the ``export`` keyword and `linksection(".gba.header")``
+// attribute are both required. The linker
+// script name, ``.gba.header``, is a convention used in zamgba to
+// locate header at linking time.
 //
 export var gameHeader linksection(".gba.header") = gba.setupROMHeader(
     "FIRST",
@@ -15,7 +21,11 @@ export var gameHeader linksection(".gba.header") = gba.setupROMHeader(
     0,
 );
 
-export fn main() linksection(".gba.main") noreturn {
+// Make sure the main() function is tagged with `export' keyword.
+// It makes the function visible in symbol table of ELF file.
+// It's required to allow the assembly code in zamgba library locate
+// the address of main() function as entry point.
+export fn main() noreturn {
     // The example https://www.coranac.com/tonc/text/first.htm
 
     // *(unsigned int*)0x04000000 = 0x0403;
@@ -30,5 +40,9 @@ export fn main() linksection(".gba.main") noreturn {
     videoRAM[136 + 80 * 240] = 0x03E0;
     videoRAM[120 + 96 * 240] = 0x7C00;
 
+    // The loop is required to match the ``noreturn`` return value.
+    // Zamgba does not handle program exit gracefully because GBA
+    // does not provide concept of graceful exit due to a lack of
+    // operating system. Make sure the while (true) {} always exist.
     while (true) {}
 }
